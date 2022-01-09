10dadc99c3d7bf1ed1091756dcc91bfa5f68bfde
matrix07012 <8049717+matrix07012@users.noreply.github.com>
2022-01-09 14:28:36 +0100

    forgot comma

--- app/serializers/rest/instance_serializer.rb
+++ app/serializers/rest/instance_serializer.rb
@@ -5,7 +5,7 @@ class REST::InstanceSerializer < ActiveModel::Serializer
 
   attributes :uri, :title, :short_description, :description, :email,
              :version, :urls, :stats, :thumbnail,
-             :languages, :max_toot_chars, :registrations, :approval_required :invites_enabled,
+             :languages, :max_toot_chars, :registrations, :approval_required, :invites_enabled,
              :configuration
 
   has_one :contact_account, serializer: REST::AccountSerializer
